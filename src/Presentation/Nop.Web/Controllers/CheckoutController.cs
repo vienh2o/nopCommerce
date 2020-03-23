8e9b468b8734134ab28613c14b5d4885bf479f05
holydk <omigoneo@gmail.com>
2020-03-23 11:35:31 +0300

    Fix the removal of the shipping address id if shipping is not required

--- src/Presentation/Nop.Web/Controllers/CheckoutController.cs
+++ src/Presentation/Nop.Web/Controllers/CheckoutController.cs
@@ -205,10 +205,6 @@ namespace Nop.Web.Controllers
         /// <param name="pickupPoint">The pickup option</param>
         protected virtual void SavePickupOption(PickupPoint pickupPoint)
         {
-            //no shipping address selected
-            _workContext.CurrentCustomer.ShippingAddressId = null;
-            _customerService.UpdateCustomer(_workContext.CurrentCustomer);
-
             var pickUpInStoreShippingOption = new ShippingOption
             {
                 Name = string.Format(_localizationService.GetResource("Checkout.PickupPoints.Name"), pickupPoint.Name),
@@ -506,11 +502,7 @@ namespace Nop.Web.Controllers
                 return Challenge();
 
             if (!_shoppingCartService.ShoppingCartRequiresShipping(cart))
-            {
-                _workContext.CurrentCustomer.ShippingAddressId = null;
-                _customerService.UpdateCustomer(_workContext.CurrentCustomer);
-                return RedirectToRoute("CheckoutShippingMethod");
-            }
+                return RedirectToRoute("CheckoutShippingMethod");        
 
             //model
             var model = _checkoutModelFactory.PrepareShippingAddressModel(prePopulateNewAddressWithCustomerFields: true);
@@ -560,11 +552,7 @@ namespace Nop.Web.Controllers
                 return Challenge();
 
             if (!_shoppingCartService.ShoppingCartRequiresShipping(cart))
-            {
-                _workContext.CurrentCustomer.ShippingAddressId = null;
-                _customerService.UpdateCustomer(_workContext.CurrentCustomer);
                 return RedirectToRoute("CheckoutShippingMethod");
-            }
             
             //pickup point
             if (_shippingSettings.AllowPickupInStore && !_orderSettings.DisplayPickupInStoreOnShippingMethodPage)
@@ -1334,9 +1322,6 @@ namespace Nop.Web.Controllers
                 }
 
                 //shipping is not required
-                _workContext.CurrentCustomer.ShippingAddressId = null;
-                _customerService.UpdateCustomer(_workContext.CurrentCustomer);
-
                 _genericAttributeService.SaveAttribute<ShippingOption>(_workContext.CurrentCustomer, NopCustomerDefaults.SelectedShippingOptionAttribute, null, _storeContext.CurrentStore.Id);
 
                 //load next step
